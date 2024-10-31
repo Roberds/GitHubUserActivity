c206ae792c8c405bdbfcd97b9e3c81b872985cbd
Roberds <robertodiazgs@gmail.com>
2024-10-31 20:33:09 +0100

    formateo de codigo

--- src/app/App.java
+++ src/app/App.java
@@ -15,73 +15,62 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Introduce el nombre de usuario de GitHub: ");
 		String username = sc.nextLine();
-				
+
 		String endponit = String.format("https://api.github.com/users/%s/events?per_page=20", username);
-		
-        ObjectMapper mapper = new ObjectMapper();
+
+		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> errorMessage = new HashMap<String, Object>();
 
+		HttpClient client = HttpClient.newHttpClient();
 
+		try {
+			HttpRequest request = HttpRequest.newBuilder().uri(new URI(endponit))
+					.header("Accept", "application/vnd.github+json").header("X-GitHub-Api-Version", "2022-11-28")
+					.timeout(Duration.ofSeconds(10)).GET().build();
 
-        HttpClient client = HttpClient.newHttpClient();
-        
-        try {
-        	HttpRequest request = HttpRequest.newBuilder()
-        			.uri(new URI(endponit))
-        			.header("Accept", "application/vnd.github+json")
-        			.header("X-GitHub-Api-Version", "2022-11-28")
-        			.timeout(Duration.ofSeconds(10))
-        			.GET()
-        			.build();
-        	
-        	HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        	
-        	if(response.body().equals("[]")){
-        		errorMessage.put("Message", "La respuesta esta vacía");
-        		errorMessage.put("Endpoint", endponit);
-        		System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorMessage));
-        	}
-        	
-        	if(response.statusCode() == 200){
-                JsonNode jsonResponse = mapper.readTree(response.body());
-                
-                salidaMapper(jsonResponse);
-                
-        	}else {
-        		errorMessage.put("ErrorCode", response.statusCode());
-        		errorMessage.put("Message", "Ha ocurrido un error");	
-        		System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorMessage));
-        	}
-			
-		} catch (URISyntaxException uriSyntaxException){
-            uriSyntaxException.printStackTrace();
-        } catch (IOException ioException){
-            ioException.printStackTrace();
-        } catch (InterruptedException interruptedException){
-            Thread.currentThread().interrupt();
-            interruptedException.printStackTrace();
-        }
-        
-        
-        
-        	
-        	
-        } 
-	
-	
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+			if (response.body().equals("[]")) {
+				errorMessage.put("Message", "La respuesta esta vacía");
+				errorMessage.put("Endpoint", endponit);
+				System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorMessage));
+			}
+
+			if (response.statusCode() == 200) {
+				JsonNode jsonResponse = mapper.readTree(response.body());
+
+				salidaMapper(jsonResponse);
+
+			} else {
+				errorMessage.put("ErrorCode", response.statusCode());
+				errorMessage.put("Message", "Ha ocurrido un error");
+				System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(errorMessage));
+			}
+
+		} catch (URISyntaxException uriSyntaxException) {
+			uriSyntaxException.printStackTrace();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+		} catch (InterruptedException interruptedException) {
+			Thread.currentThread().interrupt();
+			interruptedException.printStackTrace();
+		}
+
+	}
+
 	private static void salidaMapper(JsonNode jsonNode) {
-		
-		for(JsonNode jn : jsonNode) {
+
+		for (JsonNode jn : jsonNode) {
 			String type = jn.get("type").asText();
 			String salida;
-			
+
 			switch (type) {
 			case "PushEvent": {
 				int commit = jn.get("payload").get("commits").size();
@@ -89,7 +78,8 @@ public class App {
 				break;
 			}
 			case "CreateEvent": {
-				salida = "Opened a new " + jn.get("payload").get("ref_type") + " in " + jn.get("repo").get("name").asText();
+				salida = "Opened a new " + jn.get("payload").get("ref_type") + " in "
+						+ jn.get("repo").get("name").asText();
 				break;
 			}
 			case "WatchEvent": {
@@ -99,16 +89,10 @@ public class App {
 			default:
 				throw new IllegalArgumentException("Unexpected value: " + type);
 			}
-			
+
 			System.out.println(salida);
-			
+
 		}
 	}
-	
-	
+
 }
-
-
-
-	
-
